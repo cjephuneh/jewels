6de25059f7391a37c67ea1b113b56f5b32657c60
caleb jephuneh <92708967+cjephuneh@users.noreply.github.com>
2024-02-06 21:27:43 +0300

    Update utils.js

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,17 +1,31 @@
 import mongoose from "mongoose";
 
 const connection = {};
+const maxRetries = 5; // Set your desired number of retries
+let retries = 0;
 
 export const connectToDB = async () => {
   try {
     if (connection.isConnected) return;
 
-    // Replace the placeholder with your actual MongoDB connection string
-    const db = await mongoose.connect("mongodb+srv://erickomee:erickomee254@cluster0.axyzbpk.mongodb.net/dashboard?retryWrites=true&w=majority");
+    const db = await mongoose.connect("mongodb+srv://erickomee:erickomee254@cluster0.axyzbpk.mongodb.net/dashboard?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Add other MongoDB connection options as needed
+    });
 
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
-    console.log(error);
-    throw new Error(error);
+    console.error("MongoDB connection error:", error);
+
+    if (retries < maxRetries) {
+      const delay = Math.pow(2, retries) * 1000; // Exponential backoff in milliseconds
+      console.log(`Retrying connection in ${delay / 1000} seconds (attempt ${retries + 1} of ${maxRetries})...`);
+      retries++;
+      setTimeout(connectToDB, delay);
+    } else {
+      throw new Error("Max retries reached. Unable to establish MongoDB connection.");
+    }
   }
 };
+
